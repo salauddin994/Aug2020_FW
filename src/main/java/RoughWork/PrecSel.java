59c66ce0e12ca0ef7df48a2197e316a9020d5c43
Hassan Bhuiyan <taltektc@gmail.com>
2021-03-07 12:33:02 -0500

    added feature file for login

--- src/main/java/RoughWork/PrecSel.java
+++ src/main/java/RoughWork/PrecSel.java
@@ -1,4 +1,16 @@
 package RoughWork;
+import org.openqa.selenium.WebDriver;
+
 
 public class PrecSel {
+
+//    public WebDriver;
+//    System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+//    driver = new ChromeDriver();
+//    driver.get("http://www.google.com/");
+//    Thread.sleep(5000);  // Let the user actual
+
+
+
+
 }
